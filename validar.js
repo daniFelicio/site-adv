2717263d349a218fe45955f70151c8eaaa1ac4d0
Daniela Felício rodrigues <danidanixandi@gmail.com>
2024-12-13 15:43:17 -0300

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const loginError = document.getElementById('loginError');
+
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const username = document.getElementById('username').value.trim();
+        const password = document.getElementById('password').value.trim();
+        
+        loginError.classList.add('d-none');
+        
+        if (username === '') {
+            showError('Por favor, insira um nome de usuário.');
+            return;
+        }
+        
+        if (password === '') {
+            showError('Por favor, insira uma senha.');
+            return;
+        }
+        
+        if (password.length < 3) {
+            showError('A senha deve ter pelo 3 caracteres.');
+            return;
+        }
+        
+       
+        if (username === 'admin' && password === '123') {
+            alert('Login bem-sucedido!');
+            window.location.href = 'html/admin-dashboard.html'; 
+        } else {
+            showError('Usuário ou senha inválidos.');
+        }
+    });
+
+    function showError(message) {
+        loginError.textContent = message;
+        loginError.classList.remove('d-none');
+    }
+});
